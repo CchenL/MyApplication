43a9265d61fd5298fb20c1979a1c22547bd3a8e2
chen <cchenli0324@outlook.com>
2018-03-07 21:52:43 +0800

    第一次上传

---
+++ MyApplication/app/src/main/java/com/fragment/fragment_leave.java
@@ -0,0 +1,21 @@
+package com.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class fragment_leave extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_leave, container, false);
+
+        return view;
+    }
+}
